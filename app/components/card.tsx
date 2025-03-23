cf0e70343e1dbb1f2e220c6940c28af9038879da
Hamid-Usman <abdulhamidusman218@gmail.com>
2025-03-23 03:46:24 +0100

    enhance card component styles with gradient background and hover effects

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -36,16 +36,16 @@ interface CardType {
 const Card = ({ title, subtitle, Icon, href }: CardType) => {
   return (
     <div
-      className=" active:bg-primary translate-y-[100%] group-hover:translate-y-[0%]  w-full p-4 rounded border-[1px] border-slate-300 relative overflow-hidden group bg-white"
+      className=" active:bg-gradient-to-r active:text-white from-primary to-accent translate-y-[100%] group-hover:translate-y-[0%] w-full p-4 rounded border-[1px] border-slate-300 relative overflow-hidden group bg-white"
     >
     <div className="absolute inset-0 bg-gradient-to-r from-primary to-accent translate-y-[100%] group-hover:translate-y-[0%] transition-transform duration-300" />
 
-      <Icon className="absolute z-10 -top-12 -right-12 text-9xl text-slate-100 group-hover:text- group-hover:rotate-12 transition-transform duration-300" />
-      <Icon className="mb-2 text-2xl text-secondary group-hover:text-white transition-colors relative z-10 duration-300" />
-      <h3 className="font-medium text-lg text-slate-950 group-hover:text-white relative z-10 duration-300">
+      <Icon className="absolute z-10 -top-12 -right-12 text-9xl text-slate-100 group-hover:rotate-12 transition-transform duration-300" />
+      <Icon className="mb-2 text-2xl text-secondary  transition-colors relative z-10 duration-300" />
+      <h3 className="font-medium text-lg  group-hover:text-white relative z-10 duration-300">
         {title}
       </h3>
-      <p className="text-slate-400 group-hover:text-violet-200 relative z-10 duration-300">
+      <p className=" group-hover:text-white relative z-10 duration-300">
         {subtitle}
       </p>
     </div>
