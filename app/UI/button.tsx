daea9164b68d1172df6f0d611ee206348ee7ba65
Hamid-Usman <abdulhamidusman218@gmail.com>
2025-03-24 02:12:09 +0100

    add focus styles to button and alter link components for improved accessibility

--- app/UI/button.tsx
+++ app/UI/button.tsx
@@ -7,7 +7,7 @@ interface ButtonProp {
 export default function Button({onClick, className, children}: ButtonProp) {
     return (
         
-        <button onClick={onClick} className={`rounded-2xl border-2 border-dashed hover:border-double border-black  bg-secondary px-7 py-2 font-semibold uppercase text-black transition-all duration-300 hover:rounded-md hover:shadow-[4px_4px_0px] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none ${className}`}>
+        <button onClick={onClick} className={`rounded-2xl border-2 border-dashed hover:border-double focus:border-double border-black  bg-secondary px-7 py-2 font-semibold uppercase text-black transition-all duration-300 hover:rounded-md hover:shadow-[4px_4px_0px] focus:shadow-[4px_4px_0px]  active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none ${className}`}>
             {children}
       </button>
     )
--- app/UI/buttonAlter.tsx
+++ app/UI/buttonAlter.tsx
@@ -9,7 +9,7 @@ interface ButtonProp {
 export default function AlterLink({href, className, children}: ButtonProp) {
     return (
         
-        <Link href={href} className={`rounded-2xl border-2 border-dashed hover:border-double border-black  bg-white px-7 py-2 font-semibold uppercase text-black transition-all duration-300 hover:rounded-md hover:shadow-[4px_4px_0px] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none ${className}`}>
+        <Link href={href} className={`rounded-2xl border-2 border-dashed hover:border-double focus:border-double border-black  bg-white px-7 py-2 font-semibold uppercase text-black transition-all duration-300 hover:rounded-md focus:rounbded-md hover:shadow-[4px_4px_0px] focus:shadow-[4px_4px_0px] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none ${className}`}>
             {children}
       </Link>
     )
